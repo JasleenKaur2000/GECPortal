052f1c04c83e46d9085f64941c4f1c6f01a566fb
Deepak Garg <garg882001@gmail.com>
2022-05-01 21:37:27 +0530

    Folder Str & Routes Created

---
+++ gec_portal/src/Components/DashBoardScreen/TeacherDashBoardScreen/TeacherDsBrd.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+const TeacherDsBrd = () => {
+  return (
+    <View>
+      <Text>Tacher Dashboard</Text>
+    </View>
+  );
+};
+
+export default TeacherDsBrd;
